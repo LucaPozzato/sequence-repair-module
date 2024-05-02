74642bdf6acf2f6ad40268973c68502f61e0b6f3
Luca Pozzato <luca@pozzato.me>
2024-05-02 14:15:48 +0200

    Single process design -> passes Pre and Post Synthesis test bench
    Co-authored-by: AndreRossii <andrea.reds08@gmail.com>

--- src/design_single_process.vhd
+++ src/design_single_process.vhd
@@ -1,9 +1,3 @@
-----------------------------------------------------------------------------------
--- Project Name: Project for Logic Circuit Design
--- Authors: Luca Pozzato, Andrea Rossi
--- Year: 2023/2024
-----------------------------------------------------------------------------------
-
 library IEEE;   
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -32,57 +26,69 @@ architecture project_arch of project_reti_logiche is
     signal k: std_logic_vector(9 downto 0);
     signal addr: std_logic_vector(15 downto 0);
     signal data, cred: std_logic_vector(7 downto 0); 
-    signal next_state, current_state: state_type;
+    signal current_state: state_type;
 
     begin
-    -- change state process
-        state_reg: process(i_clk, i_rst)
+        design: process(i_clk, i_rst)
+        variable temp_addr: UNSIGNED(15 downto 0);
+        variable temp_k: UNSIGNED(9 downto 0);
+        variable temp_data: UNSIGNED(7 downto 0);
+
         begin
             if i_rst = '1' then
+                --state
                 current_state <= RST_STATE;
-                -- rising_edge needs to be last elsif
+                --lambda
+                addr <= (others => '0');
+                cred <= (others => '0');
+                k <= (others => '0');
+                data <= (others => '0');
+                --delta
+                o_done <= '0';            
+                o_mem_addr <= (others => '0');
+                o_mem_data <= (others => '0');
+                o_mem_we <= '0';
+                o_mem_en <= '0';
+
             elsif rising_edge(i_clk) then
-                current_state <= next_state;
-            end if;
-        end process;
+                --lambda
+                addr <= (others => '0');
+                cred <= (others => '0');
+                k <= (others => '0');
+                data <= (others => '0');
+                --delta
+                o_done <= '0';            
+                o_mem_addr <= (others => '0');
+                o_mem_data <= (others => '0');
+                o_mem_we <= '0';
+                o_mem_en <= '0';
 
-    -- lambda
-        lambda: process(current_state, i_start)
-            variable temp_addr: UNSIGNED(15 downto 0);
-            variable temp_k: UNSIGNED(9 downto 0);
-            -- variable temp_data: UNSIGNED(7 downto 0);
-            variable temp_data: UNSIGNED(7 downto 0);
-        -- possibilmente inizializzare segnali -> evitare latch
-        begin
-            addr <= (others => '0');
-            cred <= (others => '0');
-            k <= (others => '0');
-            data <= (others => '0');
-
-            -- if rising_edge(i_clk) then
                 case current_state is
                     when RST_STATE =>
                         if i_rst = '0' then 
-                            next_state <= WAIT_STATE;
+                            current_state <= WAIT_STATE;
                         else
-                            next_state <= RST_STATE;
+                            current_state <= RST_STATE;
                         end if;
-                        
+
                     when WAIT_STATE =>
                         if i_start = '1' then
                             k <= i_k;
                             addr <= i_add;
                             cred <= "00011111";
-                            next_state <= INIT_STATE;
+                            current_state <= INIT_STATE;
                         else 
-                            next_state <= WAIT_STATE;
+                            current_state <= WAIT_STATE;
                         end if;
-                        
-                    when INIT_STATE =>
                     
+                    when INIT_STATE =>
                         if k = "0000000000" then
-                            next_state <= DONE_STATE;
+                            --lambda
+                            current_state <= DONE_STATE;
+                            --delta
+                            o_done <= '1';
                         else
+                            --lambda
                             temp_data := UNSIGNED(data);
                             data <= std_logic_vector(temp_data);
                             temp_addr := UNSIGNED(addr);
@@ -91,10 +97,15 @@ architecture project_arch of project_reti_logiche is
                             addr <= std_logic_vector(temp_addr);
                             k <= std_logic_vector(temp_k);
                             cred <= std_logic_vector(temp_data);
-                            next_state <= WAIT_FOR_MEM_STATE;
+                            current_state <= WAIT_FOR_MEM_STATE;
+                            --delta
+                            o_mem_addr <= addr;
+                            o_mem_data <= data;
+                            o_mem_en <= '1';
                         end if;
-
+                    
                     when WAIT_FOR_MEM_STATE =>
+                        --lambda
                         temp_data := UNSIGNED(data);
                         data <= std_logic_vector(temp_data);
                         temp_addr := UNSIGNED(addr);
@@ -103,9 +114,14 @@ architecture project_arch of project_reti_logiche is
                         addr <= std_logic_vector(temp_addr);
                         k <= std_logic_vector(temp_k);
                         cred <= std_logic_vector(temp_data);
-                        next_state <= WAIT_FOR_MEM_STATE_2;
+                        current_state <= WAIT_FOR_MEM_STATE_2;
+                        --delta
+                        o_mem_addr <= addr;
+                        o_mem_data <= data;
+                        o_mem_en <= '1';
 
                     when WAIT_FOR_MEM_STATE_2 =>
+                        --lambda
                         temp_data := UNSIGNED(data);
                         data <= std_logic_vector(temp_data);
                         temp_addr := UNSIGNED(addr);
@@ -114,10 +130,14 @@ architecture project_arch of project_reti_logiche is
                         addr <= std_logic_vector(temp_addr);
                         k <= std_logic_vector(temp_k);
                         cred <= std_logic_vector(temp_data);
-                        next_state <= READ_STATE;
-                    
+                        current_state <= READ_STATE;
+                        --delta
+                        o_mem_addr <= addr;
+                        o_mem_data <= data;
+                        o_mem_en <= '1';
+
                     when READ_STATE =>
-                        -- temp_data := UNSIGNED(cred);
+                        --lambda
                         temp_addr := UNSIGNED(addr);
                         addr <= std_logic_vector(addr);
                         
@@ -143,105 +163,63 @@ architecture project_arch of project_reti_logiche is
                         temp_k := UNSIGNED(k) - 1;
                         k <= std_logic_vector(temp_k);
                         
-                        next_state <= WRITE_MEM_STATE;
-                    
-                    when WRITE_MEM_STATE =>
-                    -- scrivo 
-                        temp_addr := UNSIGNED(addr) + 1;
-                        addr <= std_logic_vector(temp_addr);
-                        temp_data := UNSIGNED(data);
-                        data <= std_logic_vector(temp_data);
-                        temp_k := UNSIGNED(k);
-                        k <= std_logic_vector(temp_k);
-                        temp_data := UNSIGNED(cred);
-                        cred <= std_logic_vector(temp_data);
-                        next_state <= WRITE_CRED_STATE;
-                    
-                    when WRITE_CRED_STATE =>
-                    -- scrivo cred
-                        temp_addr := UNSIGNED(addr) + 1;
-                        addr <= std_logic_vector(temp_addr);
-                        temp_data := UNSIGNED(data);
-                        data <= std_logic_vector(temp_data);
-                        temp_k := UNSIGNED(k);
-                        k <= std_logic_vector(temp_k);
-                        temp_data := UNSIGNED(cred);
-                        cred <= std_logic_vector(temp_data);
-                        next_state <= INIT_STATE;
-
-                    when DONE_STATE =>
-                        if i_start = '0' then
-                            next_state <= WAIT_STATE;
-                        else
-                            next_state <= DONE_STATE;
-                    end if;
-
-                    when others =>
-                        next_state <= RST_STATE;
-
-                end case;
-            -- end if;
-        end process;
-
-    -- delta
-    delta: process(current_state, i_start)
-    begin
-        -- if rising_edge(i_clk) then
-            o_done <= '0';            
-            o_mem_addr <= (others => '0');
-            o_mem_data <= (others => '0');
-            o_mem_we <= '0';
-            o_mem_en <= '0';
-
-            case current_state is
-                when RST_STATE =>
-                    
-                when WAIT_STATE =>
-                
-                when INIT_STATE =>
-                    if UNSIGNED(k) = 0 then
-                        o_done <= '1';
-                    else 
+                        current_state <= WRITE_MEM_STATE;
+                        --delta
                         o_mem_addr <= addr;
                         o_mem_data <= data;
                         o_mem_en <= '1';
-                    end if;
-                
-                when WAIT_FOR_MEM_STATE =>
-                    o_mem_addr <= addr;
-                    o_mem_data <= data;
-                    o_mem_en <= '1';
+                        o_mem_we <= '1';
 
-                when WAIT_FOR_MEM_STATE_2 =>
-                    o_mem_addr <= addr;
-                    o_mem_data <= data;
-                    o_mem_en <= '1';
-                
-                when READ_STATE =>
-                    o_mem_addr <= addr;
-                    o_mem_data <= data;
-                    o_mem_en <= '1';
-                    o_mem_we <= '1';
-                
-                when WRITE_MEM_STATE =>
-                    o_mem_addr <= addr;
-                    o_mem_data <= data;
-                    o_mem_en <= '1';
-                    o_mem_we <= '1';
+                    when WRITE_MEM_STATE =>
+                        --lambda
+                        temp_addr := UNSIGNED(addr) + 1;
+                        addr <= std_logic_vector(temp_addr);
+                        temp_data := UNSIGNED(data);
+                        data <= std_logic_vector(temp_data);
+                        temp_k := UNSIGNED(k);
+                        k <= std_logic_vector(temp_k);
+                        temp_data := UNSIGNED(cred);
+                        cred <= std_logic_vector(temp_data);
+                        current_state <= WRITE_CRED_STATE;
+                        --delta
+                        o_mem_addr <= addr;
+                        o_mem_data <= data;
+                        o_mem_en <= '1';
+                        o_mem_we <= '1';
 
-                when WRITE_CRED_STATE =>
-                    o_mem_addr <= addr;
-                    o_mem_data <= cred;
-                    o_mem_en <= '1';
-                    o_mem_we <= '1';
-                    
-                when DONE_STATE =>
-                    o_done <= '1';
+                    when WRITE_CRED_STATE =>
+                        --lambda
+                        temp_addr := UNSIGNED(addr) + 1;
+                        addr <= std_logic_vector(temp_addr);
+                        temp_data := UNSIGNED(data);
+                        data <= std_logic_vector(temp_data);
+                        temp_k := UNSIGNED(k);
+                        k <= std_logic_vector(temp_k);
+                        temp_data := UNSIGNED(cred);
+                        cred <= std_logic_vector(temp_data);
+                        current_state <= INIT_STATE;
+                        --delta
+                        o_mem_addr <= addr;
+                        o_mem_data <= cred;
+                        o_mem_en <= '1';
+                        o_mem_we <= '1';
 
-                when others =>
+                    when DONE_STATE =>
+                        --lambda
+                        if i_start = '0' then
+                            current_state <= WAIT_STATE;
+                        else
+                            current_state <= DONE_STATE;
+                        end if;
+                        --delta
+                        o_done <= '1';
 
-            end case;
-        -- end if;
-    end process;
-        
+                    when others =>
+                        current_state <= RST_STATE;
+
+                end case;
+
+            end if;
+
+        end process;
 end project_arch;
